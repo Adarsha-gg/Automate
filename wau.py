801ecf61bc5fabe502f80146143420db28097d85
Adarsha-gg <adarshamishra33@gmail.com>
2023-01-03 19:18:03 +0545

    get reddit posts ezpz

---
+++ wau.py
@@ -0,0 +1,11 @@
+import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("<the name of the reddot>")
+
+for submission in subreddit.hot(limit=4000):
+    if "keywords or whatever" in submission.title:
+        print("Title: ", submission.title)
+        print("Text: ", submission.selftext)
+        print("---------------------------------\n")
