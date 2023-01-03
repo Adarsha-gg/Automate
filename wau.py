4b67b5b0429ee410739af34fd8772a7f0b760de5
Adarsha-gg <adarshamishra33@gmail.com>
2023-01-03 19:20:56 +0545

    wau

--- wau.py
+++ wau.py
@@ -5,7 +5,7 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("<the name of the reddot>")
 
 for submission in subreddit.hot(limit=4000):
-    if "keywords or whatever" in submission.title:
+    if "keywords " in submission.title:
         print("Title: ", submission.title)
         print("Text: ", submission.selftext)
         print("---------------------------------\n")
